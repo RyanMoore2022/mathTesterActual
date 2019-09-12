1cac2023f5a194cf983570927211f83f6da93c47
Ryan Moore <ryan.moore.2022@student.lhschools.org>
2019-09-12 10:00:59 -0400

    Added equations

---
+++ src/mathTesterActual.java
@@ -0,0 +1,44 @@
+public class mathTesterActual {
+    public static void main(String[] args) {
+        String firstWord;
+        String secondWord;
+        int firstNumber, secondNumber;
+
+
+        firstWord = "dog";
+        secondWord = "house";
+        firstNumber = 5;
+        secondNumber = 10;
+
+
+        System.out.println(firstWord + secondWord);
+        System.out.println(firstWord + (firstNumber + secondNumber));
+        System.out.println("firstNumber + secondNumber is " + (firstNumber + secondNumber));
+        System.out.println("firstNumber - secondNumber is " + (firstNumber - secondNumber));
+        System.out.println("firstNumber / secondNumber is " + (firstNumber / secondNumber));
+        System.out.println("firstNumber * secondNumber is " + (firstNumber * secondNumber));
+        System.out.println("This is the modulus of the secondNumber by the firstNumber is " + (firstNumber % secondNumber));
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
