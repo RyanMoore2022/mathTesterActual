e6832b83367c163e8d813a620522c273cae4dddf
Ryan Moore <ryan.moore.2022@student.lhschools.org>
2019-09-12 10:16:45 -0400

    updated equation to a better example

--- src/mathTesterActual.java
+++ src/mathTesterActual.java
@@ -15,9 +15,14 @@ public class mathTesterActual {
         System.out.println(firstWord + (firstNumber + secondNumber));
         System.out.println("firstNumber + secondNumber is " + (firstNumber + secondNumber));
         System.out.println("firstNumber - secondNumber is " + (firstNumber - secondNumber));
-        System.out.println("firstNumber / secondNumber is " + (firstNumber / secondNumber));
+        System.out.println("secondNumber / firstNumber is " + (secondNumber / firstNumber));
         System.out.println("firstNumber * secondNumber is " + (firstNumber * secondNumber));
         System.out.println("This is the modulus of the secondNumber by the firstNumber is " + (firstNumber % secondNumber));
+
+
+
+
+
 
 
     }
