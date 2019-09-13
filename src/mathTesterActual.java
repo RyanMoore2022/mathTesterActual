50e09193e8075a92a2231ea13efc05678a56be6a
Ryan Moore <ryan.moore.2022@student.lhschools.org>
2019-09-13 09:33:51 -0400

    added speaking notes for the screencast

--- src/mathTesterActual.java
+++ src/mathTesterActual.java
@@ -1,16 +1,19 @@
 public class mathTesterActual {
     public static void main(String[] args) {
+
+      //declaring the variables
         String firstWord;
         String secondWord;
         int firstNumber, secondNumber;
         int total;
 
+        //giving the variable elements
         firstWord = "dog";
         secondWord = "house";
         firstNumber = 5;
         secondNumber = 10;
 
-
+        //running the code
         System.out.println(firstWord + secondWord);
         System.out.println(firstWord + (firstNumber + secondNumber));
         System.out.println("firstNumber + secondNumber is " + (firstNumber + secondNumber));
@@ -22,6 +25,7 @@ public class mathTesterActual {
         total = firstNumber+secondNumber/secondNumber;
 
         System.out.println("the total of firsNumber + secondNumber / secondNumber is " +total);
+
 
 
 
